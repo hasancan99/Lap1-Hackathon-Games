802b58b4e281568f2d89f42c01c805e66789d796
Hasancan <hasancan_cifci@hotmail.co.uk>
2023-06-09 12:07:43 +0100

    added the rules to the game, as well as a basic initial interface which asks the user if they want to change their card

--- Card_Game/index.js
+++ Card_Game/index.js
@@ -8,6 +8,9 @@
     The player with the highest card wins.
 
 */
+
+const readline = require('readline')
+
 const deckGen = ()=>{
     return Array.from({length:52}, (_,i)=> i+1)
     
@@ -24,16 +27,46 @@ const shuffleDeck= deck=>{
     return shuffle
 }
 
-const game = deck =>{
+const game = async (deck) =>{
     let playerCard = deck.splice(0,1)[0]
     let oppCard = deck.splice(0,1)[0]
-    console.log(playerCard)
-    console.log(oppCard)
-    console.log(deck)
+    console.log(`You card is ${playerCard}, would you like to draw a new card? (y/n)`)
 
+    readline.emitKeypressEvents(process.stdin)
+    if (process.stdin.setRawMode !=null ) process.stdin.setRawMode(true)
+    //this listens to key press
+    process.stdin.on("keypress",(str,key)=>{
+        if(key.name =="y"){
+            process.stdin.pause()
+            playerCard = deck.splice(0,1)[0]
+            console.log(playerCard)
+            winLose(playerCard,oppCard)}
+        else{
+            process.stdin.pause()
+            winLose(playerCard,oppCard)}
+        })
+    
+
+    
+    
+    
+}
+const winLose=(player,opp)=>{
+    if (player>opp){
+        console.log("You win!")
+        console.log(`You had : ${player}`)
+        console.log(`Opponent had ${opp}`)
+    }else if(player==opp){
+        console.log("draw")
+        console.log(`You had : ${player}`)
+        console.log(`Opponent had ${opp}`)
+    }else{console.log("YOU LOSE!") }
+        console.log(`You had : ${player}`)
+        console.log(`Opponent had ${opp}`)
 }
 
 let deck = deckGen()
+console.log(deck)
 
 deck = shuffleDeck(deck)
 game(deck)
